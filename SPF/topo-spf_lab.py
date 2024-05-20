f8d20b71edebe5d4ff0184928720f5b9ec43b8c5
Achmad Basuki <9971912+abazh@users.noreply.github.com>
2024-05-20 11:10:52 +0700

    Topology changed to 3 switches with loop.

--- SPF/topo-spf_lab.py
+++ SPF/topo-spf_lab.py
@@ -9,7 +9,7 @@ from mininet.cli import CLI
 from functools import partial
 
 class MyTopo( Topo ):
-    "Tugas SDN04 Finding Shortest Paths."
+    "Network topology with loops and suitable for finding Shortest Paths."
 
     def addSwitch( self, name, **opts ):
         kwargs = { 'protocols' : 'OpenFlow13'}
@@ -30,30 +30,40 @@ class MyTopo( Topo ):
         h5 = self.addHost( 'h5',ip='10.3.0.5/8' )
         h6 = self.addHost( 'h6',ip='10.3.0.6/8' )
 
-	# Add switches
+	    # Add switches
         s1 = self.addSwitch( 's1' )
         s2 = self.addSwitch( 's2' )
         s3 = self.addSwitch( 's3' )
-        s4 = self.addSwitch( 's4' )
-        s5 = self.addSwitch( 's5' )
-        s6 = self.addSwitch( 's6' )
 
         # Add links host to switch
-        self.addLink( s1, h1, 1, 1 )
-        self.addLink( s2, h2, 1, 1 )
-        self.addLink( s3, h3, 1, 1 )
-        self.addLink( s4, h4, 1, 1 )
-        self.addLink( s5, h5, 1, 1 )
-        self.addLink( s6, h6, 1, 1 )
+        self.addLink( s1, h1, port1=1, port2=1 )
+        self.addLink( s1, h2, port1=2, port2=1 )
+        self.addLink( s2, h3, port1=1, port2=1 )
+        self.addLink( s2, h4, port1=2, port2=1 )
+        self.addLink( s3, h5, port1=1, port2=1 )
+        self.addLink( s3, h6, port1=2, port2=1 )
         # Add links switch to switch
-        self.addLink( s1, s2, 2, 2 )
-        self.addLink( s1, s3, 3, 2 )
-        self.addLink( s2, s4, 3, 2 )
-        self.addLink( s2, s5, 4, 2 )
-        self.addLink( s3, s4, 3, 3 )
-        self.addLink( s3, s6, 4, 2 )
-        self.addLink( s4, s5, 4, 3 )
-        self.addLink( s4, s6, 5, 3 )
+        self.addLink( s1, s2, port1=3, port2=3 )
+        self.addLink( s2, s3, port1=4, port2=4 )
+        self.addLink( s3, s1, port1=3, port2=4 )
+    """
+    The representation of the topology
+    The value in () represent the port number in the switch
+               h6
+               |
+               |
+              (2)
+    h5 ----(1) s3 (4) ----------+
+              (3)               |
+               |                |
+               |                |
+              (4)              (4)
+    h1 ----(1) s1 (3) -----(3) s2 (2) --- h4   
+              (2)              (1)
+               |                |
+               |                |
+               h2               h3
+    """
 
 def run():
     "The Topology for Server - Round Robin LoadBalancing"
@@ -77,4 +87,3 @@ def run():
 if __name__ == '__main__':
     setLogLevel( 'info' )
     run()
-
